057f579ba607961edadb48945c14ce803b1e2980
Rangding Zhang <love-nankai@163.com>
2019-06-05 17:41:43 +0800

    Create stack_test.go

---
+++ base/stack/stack_test.go
@@ -0,0 +1,27 @@
+package stack
+ 
+import "testing"
+ 
+func Test(t *testing.T) {
+	stack := New()
+	const total = 10
+	for i := 0; i < total; i++ {
+		stack.Push(i)
+	}
+	for i := total - 1; i > -1; i-- {
+		item := stack.Pop()
+		if item != i {
+			t.Error("failed")
+		}
+	}
+}
+ 
+func Benchmark(b *testing.B) {
+	stack := New()
+	for i := 0; i < b.N; i++ {
+		stack.Push(i)
+	}
+	for i := b.N - 1; i > -1; i-- {
+		stack.Pop()
+	}
+}
