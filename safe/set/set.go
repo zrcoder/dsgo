54d714235e37258c76d93e1fe9e40794ca44b222
Rangding Zhang <love-nankai@163.com>
2019-06-05 17:42:51 +0800

    Create set.go

---
+++ safe/set/set.go
@@ -0,0 +1,58 @@
+package set
+ 
+import (
+	"sync"
+ 
+	. "github.com/zrcoder/dsGo"
+	base "github.com/zrcoder/dsGo/base/set"
+)
+ 
+type Set struct {
+	lock sync.RWMutex
+	set  base.Set
+}
+ 
+func New() *Set {
+	s := &Set{}
+	s.set = base.New()
+	return s
+}
+ 
+func NewWithCapacity(c int) *Set {
+	s := &Set{}
+	s.set = base.NewWithCapacity(c)
+	return s
+}
+ 
+func (s *Set) Add(item Any) {
+	s.lock.Lock()
+	s.set.Add(item)
+	s.lock.Unlock()
+}
+ 
+func (s *Set) Delete(item Any) {
+	s.lock.Lock()
+	s.set.Delete(item)
+	s.lock.Unlock()
+}
+ 
+func (s *Set) Exist(item Any) bool {
+	s.lock.RLock()
+	exist := s.set.Exist(item)
+	s.lock.RUnlock()
+	return exist
+}
+ 
+func (s *Set) Count() int {
+	s.lock.RLock()
+	length := s.set.Count()
+	s.lock.RUnlock()
+	return length
+}
+ 
+func (s *Set) AllItems() []Any {
+	s.lock.RLock()
+	items := s.set.AllItems()
+	s.lock.RUnlock()
+	return items
+}
