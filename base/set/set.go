1f32dc5c5b167c4c20f35a8dc3a5e191892991ed
Rangding Zhang <love-nankai@163.com>
2019-06-05 17:38:42 +0800

    Create set.go

---
+++ base/set/set.go
@@ -0,0 +1,38 @@
+package set
+
+import . "code.huawei.com/interest/dsGo"
+
+type Set map[Any]Empty
+
+func New() Set {
+	return make(map[Any]Empty)
+}
+
+func NewWithCapacity(c int) Set  {
+	return make(map[Any]Empty, c)
+}
+
+func (s Set) Add(item Any) {
+	s[item] = Empty{}
+}
+
+func (s Set) Delete(item Any) {
+	delete(s, item)
+}
+
+func (s Set) Exist(item Any) bool {
+	_, ok := s[item]
+	return ok
+}
+
+func (s Set) Count() int {
+	return len(s)
+}
+
+func (s Set) AllItems() []Any {
+	r := make([]Any, s.Count())
+	for k := range s {
+		r = append(r, k)
+	}
+	return r
+}
