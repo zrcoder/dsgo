a4164880f5d7c91c8b6e63b7622cc530c64d5899
Rangding Zhang <love-nankai@163.com>
2019-06-06 18:28:22 +0800

    Create queue.go

---
+++ safe/queue/queue.go
@@ -0,0 +1,36 @@
+/*
+	A queue gives you a FIFO or first-in firs-out order.
+	The element you inserted first is also the first one to come out again.
+*/
+package queue
+
+import (
+	"sync"
+
+	. "code.huawei.com/interest/dsGo"
+	base "code.huawei.com/interest/dsGo/base/queue"
+)
+
+type Queen struct {
+	sync.Mutex
+	queue *base.Queen
+}
+
+func New() *Queen {
+	return &Queen{queue: base.New()}
+}
+
+// put a given item into the queue
+func (q *Queen) Enqueue(item Any) {
+	q.Lock()
+	q.queue.Enqueue(item)
+	q.Unlock()
+}
+
+// remove the first item from the queue and returns it
+func (q *Queen) Dequeue() Any {
+	q.Lock()
+	item := q.queue.Dequeue()
+	q.Unlock()
+	return item
+}
