5461415b85f01672d5e31df4230b82c7b7391eb3
Rangding Zhang <love-nankai@163.com>
2019-06-06 18:16:03 +0800

    Update stack.go

--- base/stack/stack.go
+++ base/stack/stack.go
@@ -1,3 +1,9 @@
+/*
+	A stack gives you a LIFO or last-in first-out order.
+	You can only Push to add a new element to the top of the stack,
+	Pop to remove the element from the top,
+	and Peek at the top element without poping it off.
+*/
 package stack
  
 import . "github.com/zrcoder/dsGo"
